f81666b924919862d335e2a5091a8607ab7abd0e
DoomLord <yinfei1224@163.com>
2016-12-22 17:39:23 +0800

    加入第二个Activity

--- app/src/main/java/com/bigeshow/quiz/MainActivity.java
+++ app/src/main/java/com/bigeshow/quiz/MainActivity.java
@@ -1,7 +1,9 @@
 package com.bigeshow.quiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -15,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton mNextBtn;
     private ImageButton mPreviousBtn;
     private TextView mQuestionTextView;
+    private Button mCheatBtn;
+    private static final String TAG="QuizActivity";
+    private static final String KEY_INDEX="index";
+    private static final int REQUEST_CODE_CHEAT = 0;
+    private boolean mIsCheater;
     private Question[] mQuestions=new Question[]{
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, false),
@@ -26,16 +33,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: called");
         setContentView(R.layout.activity_main);
         mTrueBtn=(Button) findViewById(R.id.true_button);
         mFalseBtn=(Button) findViewById(R.id.false_button);
         mQuestionTextView=(TextView) findViewById(R.id.question_text_view);
+        mIsCheater=false;
+        if(savedInstanceState!=null){
+            mCurrentIndex=savedInstanceState.getInt(KEY_INDEX);
+        }
         UpdateQuestion();
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mCurrentIndex=(mCurrentIndex+1)%mQuestions.length;
                 UpdateQuestion();
+                mIsCheater=false;
             }
         });
         mTrueBtn.setOnClickListener(new View.OnClickListener() {
@@ -58,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 mCurrentIndex=(mCurrentIndex+1)%mQuestions.length;
                 UpdateQuestion();
+                mIsCheater=false;
             }
         });
 
@@ -67,11 +81,59 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 mCurrentIndex=(mCurrentIndex+mQuestions.length-1)%mQuestions.length;
                 UpdateQuestion();
+                mIsCheater=false;
+            }
+        });
+
+        mCheatBtn=(Button) findViewById(R.id.cheat_button);
+        mCheatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=CheatActivity.newIntent(MainActivity.this,mQuestions[mCurrentIndex].isAnswerTrue());
+                startActivityForResult(i,REQUEST_CODE_CHEAT);
             }
         });
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState: called");
+        outState.putInt(KEY_INDEX,mCurrentIndex);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: called");
+    }
+
     private void UpdateQuestion() {
+        //Log.d(TAG, "UpdateQuestion: "+mCurrentIndex,new Exception());
         mQuestionTextView.setText(mQuestions[mCurrentIndex].getTextResId());
     }
 
@@ -79,11 +141,30 @@ public class MainActivity extends AppCompatActivity {
     {
         boolean answer=mQuestions[mCurrentIndex].isAnswerTrue();
         int messageResId;
-        if (answer==userPressTrue){
-            messageResId=R.string.correct_toast;
+        if(mIsCheater){
+            messageResId=R.string.judgment_toast;
         }else{
-            messageResId=R.string.incorrect_toast;
+            if (answer==userPressTrue){
+                messageResId=R.string.correct_toast;
+            }else{
+                messageResId=R.string.incorrect_toast;
+            }
         }
+
         Toast.makeText(MainActivity.this,messageResId,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode!=RESULT_OK)
+        {
+            return;
+        }
+        if (requestCode==REQUEST_CODE_CHEAT){
+            if(data==null){
+                return;
+            }
+            mIsCheater=CheatActivity.wasAnswerShown(data);
+        }
     }
 }
